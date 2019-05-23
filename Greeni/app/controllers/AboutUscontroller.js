d0250879ddcc8d4e180b44d1a5274f9990de7939
shahraeinisepehr@gmail.com <shahraeinisepehr@gmail.com>
2019-05-23 20:44:29 +0430

    11111111

---
+++ Greeni/app/controllers/AboutUscontroller.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('AboutUscontroller', AboutUscontroller);
+
+    AboutUscontroller.$inject = ['$location'];
+
+    function AboutUscontroller($location) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'AboutUscontroller';
+
+        activate();
+
+        function activate() { }
+    }
+})();
