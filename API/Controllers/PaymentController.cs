8eada7a5cf4568999c1cad10120291e25949aec6
Vahid <v.moghaddam59@gmail.com>
2020-03-22 18:01:27 +0430

    api

---
+++ API/Controllers/PaymentController.cs
@@ -0,0 +1,51 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.AspNet.OData;
+
+using System.Web.Http.Description;
+using System.Collections.Generic;
+using System;
+using System.Data.Entity.Validation;
+using System.Web.Http.Cors;
+using System.Web.Http.ModelBinding;
+using API.Repositories;
+using API.Models;
+using System.Text;
+using System.Configuration;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace API.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class PaymentController : ApiController
+    {
+        private UnitOfWork unitOfWork = new UnitOfWork();
+
+        public void CheshmaieKooretRoBazKon()
+        {
+            //ایجاد یک نمونه از سرویس درخواست پرداخت قبض درگاه پرداخت اینترنتی پارسیان
+            
+            using (var service = new ParsianRef.SaleServiceSoapClient())
+            {
+
+            }
+
+
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                //db.Dispose();
+                unitOfWork.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
