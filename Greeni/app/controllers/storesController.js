4a779bcf533cd983f5060d5dd1c2aea3b8bcfe51
Vahid <v.moghaddam59@gmail.com>
2020-04-02 19:42:27 +0430

    876876

---
+++ Greeni/app/controllers/storesController.js
@@ -0,0 +1,30 @@
+﻿'use strict';
+app.controller('storesController', ['$scope', '$routeParams', '$location', 'authService', 'ngAuthSettings', '$rootScope', function ($scope, $routeParams, $location, authService, ngAuthSettings, $rootScope) {
+
+    
+    /////////////////////////////////////////
+    $scope.loadingVisible = false;
+    $scope.loadPanel = {
+        message: 'لطفا صبر کنید',
+        rtlEnabled: true,
+        showIndicator: true,
+        showPane: true,
+        shading: true,
+        closeOnOutsideClick: false,
+        shadingColor: "rgba(0,0,0,0.4)",
+        // position: { of: "body" },
+        onShown: function () {
+
+        },
+        onHidden: function () {
+
+        },
+        bindingOptions: {
+            visible: 'loadingVisible'
+        }
+    };
+
+    ////////////////////////////////////////////
+
+
+}]);
